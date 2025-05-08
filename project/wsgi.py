047ec7db88613c529f85e6334d3376c506a465c4
takahasi-hashi <hken7416@gmail.com>
2025-05-08 20:33:36 +0900

    rewrite wsgi.py

--- project/wsgi.py
+++ project/wsgi.py
@@ -4,12 +4,15 @@ WSGI config for project project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/  
 """
 
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
