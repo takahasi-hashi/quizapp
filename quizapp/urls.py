7e9d20eb636ec8717b54b226bd61d85dfe58daf1
takahasi-hashi <hken7416@gmail.com>
2025-05-09 21:28:11 +0900

    Add urls.py

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
 
 app_name = 'quiz'
 
