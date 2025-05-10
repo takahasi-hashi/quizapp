bda48a8e01e0f7c2b8288ed79641059774c22282
takahasi-hashi <hken7416@gmail.com>
2025-05-10 21:13:51 +0900

    Fix: updated settings and render.yaml for deployment

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -5,7 +5,6 @@ from django.shortcuts import redirect
 app_name = 'quizapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', quizapp_views.index_view, name='index'),  # ← ここで "/" をトップに
     path('question/<int:q_number>/', views.question_view, name='question'),
     path('result/', views.result_view, name='result'),
